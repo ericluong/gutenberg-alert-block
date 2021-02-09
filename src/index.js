1b2d968889d0f07fbec82c2ab8215768ead67f08
Eric Luong <eric@riseup.ai>
2021-02-09 16:24:56 +0100

    Styling component (WIP)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import {registerBlockType} from '@wordpress/blocks';
 import {useBlockProps} from '@wordpress/block-editor';
+import {Icon} from '@wordpress/components';
 
-import './index.scss';
-import './style.scss';
+import './scss/index.scss';
+import './scss/style.scss';
 
 registerBlockType('gutenberg-alert-block/info', {
     apiVersion: 2,
@@ -14,7 +15,15 @@ registerBlockType('gutenberg-alert-block/info', {
         return <div {...blockProps}>Info Alert Block</div>
     },
     save: () => {
-        const blockProps = useBlockProps.save();
-        return <div {...blockProps}>Info Alert Block</div>
+        const blockProps = useBlockProps.save({class: "gutenberg-alert-block gutenberg-alert-block-info"});
+        return <div {...blockProps}>
+            <Icon icon="info"/>
+            <div class="gutenberg-alert-block-text">
+                <p class="gutenberg-alert-block-heading">Title
+                </p>
+                <p class="gutenberg-alert-block-content">Content
+                </p>
+            </div>
+        </div>
     }
 });
