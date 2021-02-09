8ae4f1fb21cf25168ac5ffbecbee6be217e3439d
Eric Luong <eric@riseup.ai>
2021-02-09 13:31:56 +0100

    Add entry file

---
+++ gutenberg-alert-block.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Gutenberg Alert Block
+ * Description: Display alert blocks in Gutenberg editor.
+ * Version: 1.0
+ * Author: Eric Luong
+ * Author URI: http://ericluong.fr/
+ * Text Domain: gutenberg-alert-block
+ */
